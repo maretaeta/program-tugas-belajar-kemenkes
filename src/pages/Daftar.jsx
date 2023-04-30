7c885efd11a7f747bc2681ec7920b2a67c16a0e9
amiqbal <iqbalmuhammad7578@gmail.com>
2023-05-01 00:40:04 +0700

    update API page daftar

--- src/pages/Daftar.jsx
+++ src/pages/Daftar.jsx
@@ -1,21 +1,47 @@
 import React, { useState, useEffect } from "react";
-import axios  from 'axios';
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const Daftar = () => {
   const [value, setValue] = useState("");
   const [provs, setProvs] = useState([]);
+  const [data, setData] = useState([]);
 
+  const handleSelectChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const Alert = () => {
+    Swal.fire("Good job!", "Data Anda Sudah Tersimpan !", "success");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // kirim data form ke server atau lakukan proses lainnya
+    // setelah proses selesai, panggil fungsi untuk merefresh halaman
+    window.location.reload();
+  };
+
+  //   useEffect(() => {
+  //   axios
+  //     .get(`https://dev.farizdotid.com/api/daerahindonesia/provinsi`)
+  //     .then((response) => {
+  //       setProvs(response.data);
+  //       console.log(provs); // tambahkan ini untuk memeriksa nilai dari variable provs
+  //     });
+  // });
   useEffect(() => {
     axios
-      .get(`https://dev.farizdotid.com/api/daerahindonesia/provinsi`)
-      .then((response) => {
-        setProvs(response.data.result);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      .get(
+        "https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json"
+      )
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
   }, []);
-  
+
+  //removing duplicate country names
+  let country = [...new Set(data.map((item) => item.country))];
+  country.sort();
+
   return (
     <div className="max-w-7xl md:mx-auto mx-5 ">
       <div className="my-16">
@@ -24,7 +50,7 @@ const Daftar = () => {
         </h1>
         <p>Dibuka : 26 April 2023 | Ditutup : 03 Mei 2023</p>
       </div>
-      <form className="container bg-slate-300">
+      <form className="container bg-slate-300" onConfirm={handleSubmit}>
         <div className="">
           <h1 className="bg-blue-600 mb-4 pl-5">Formulir Pendaftaran Online</h1>
         </div>
@@ -43,18 +69,18 @@ const Daftar = () => {
           <div className="grid grid-cols-2 border-b border-b-gray-600 ">
             <div className="mx-5">
               <p>Status Pelamar * :</p>
-              <p className="text-red-600">Requester status</p>
+              <p className="text-red-600 text-sm italic">* Requester status</p>
             </div>
             <div className="">
               <select
-                className="rounded-sm border-gray-300 border-2 block mt-2"
+                className="rounded-sm border-gray-300 border-2 block mt-2 "
                 id="question"
                 onChange={(e) => setValue(e.target.value)}
                 value={value}
               >
-                <option value="">Pilih Status</option>
-                <option value={"true"}>Parsial</option>
-                <option value={"false"}>Baru</option>
+                <option>Pilih Status</option>
+                <option>Parsial</option>
+                <option>Baru</option>
               </select>
             </div>
           </div>
@@ -193,52 +219,200 @@ const Daftar = () => {
               <p>Jenjang Pendidikan * :</p>
               <p className="text-red-600 text-sm italic">* yang diinginkan</p>
             </div>
-            <div className="">
+            <div className="mx-4">
               <select
-                className="rounded-sm border-gray-300 border-2 block mt-2"
-                id="question"
-                onChange={(e) => setValue(e.target.value)}
-                value={value}
+                id="jenjang"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-64 p-1"
               >
                 <option value="">Pilih Jenjang</option>
-                <option value=""> D-IV</option>
-                <option value="">D-IV dengan profesi</option>
-                <option value="">S-1</option>
-                <option value="">S-1 dengan profesi (non dokter)</option>
-                <option value="">Profesi (non dokter)</option>
-                <option value="">S-2</option>
-                <option value="">S-2 dengan spesialis Keperawatan</option>
-                <option value="">Spesialis Keperawatan</option>
-                <option value="">S-3 (Dosen, WI, Peneliti)</option>
-                <option value="">Doble Degree</option>
+                <option value="D-IV">D-IV</option>
+                <option value="D-IV dengan profesi">D-IV dengan profesi</option>
+                <option value="S-1">S-1</option>
+                <option value="S-1 dengan profesi (non dokter)">
+                  S-1 dengan profesi (non dokter)
+                </option>
+                <option value="Profesi (non dokter)">
+                  Profesi (non dokter)
+                </option>
+                <option value="S-2">S-2</option>
+                <option value="S-2 dengan spesialis Keperawatan">
+                  S-2 dengan spesialis Keperawatan
+                </option>
+                <option value="Spesialis Keperawatan">
+                  Spesialis Keperawatan
+                </option>
+                <option value="S-3 (Dosen, WI, Peneliti)">
+                  S-3 (Dosen, WI, Peneliti)
+                </option>
+                <option value="Doble Degree">Doble Degree</option>
               </select>
             </div>
           </div>
-          {provs.map((q) =>(
-            <div className="grid grid-cols-2 pt-5" key={q.id}>
-            <div className="mx-5">
-              <p>Jenjang Pendidikan * :</p>
-              <p className="text-red-600 text-sm italic">* yang diinginkan</p>
-            </div>
-            <div className="" >
+          <div className="grid grid-cols-2 pt-5 ">
+            <p className="mx-4">Asal Daerah :</p>
+            <div className="mx-4">
               <select
-                className="rounded-sm border-gray-300 border-2 block mt-2"
-                id="question"
-                onChange={(e) => setValue(e.target.value)}
-                value={value}
+                id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-64 p-1"
               >
-                <option key={q.id}>{p.name}</option>
+                <option>Select Country</option>
+                {country?.map((item, index) => (
+                  <option key={index} value={item}>
+                    {item}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
-
-          ))}
-          
         </div>
         <div className="pt-5">
-          <h1 className="bg-blue-600 mb-4 pl-5">Persayaratan Status Kepegawaian</h1>
+          <h1 className="bg-blue-600 mb-4 pl-5">
+            Persayaratan Status Kepegawaian
+          </h1>
+        </div>
+        <div className="grid grid-cols-2 pt-5 border-b pb-5 border-b-gray-600">
+          <div className="mx-4">
+            <p  className="">Jenis Pegawai :</p>
+            <p className="text-red-500 text-sm italic">* yang diinginkan</p>
+          </div>
+          <div className="mx-4">
+            <select
+              id="countries"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-64 p-1"
+            >
+              <option value="">Jabatan Struktural</option>
+              <option value="Jabatan Struktural"> Jabatan Struktural</option>
+              <option value="Fungsional Umum">Fungsional Umum</option>
+              <option value="Fungsional Tertentu">fungsional Tertentu</option>
+              <option value="WI/Dosen">WI/Dosen</option>
+              <option value="Peneliti">Peneliti</option>
+            </select>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 pt-5 border-b pb-5 border-b-gray-600">
+          <p className="mx-4">Jenis SDM Kesehatan :</p>
+          <div className="mx-4">
+            <select
+              id="countries"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-64 p-1"
+            >
+              <option value="">Jenis Pegawai</option>
+              <option value="Jabatan Struktural"> Tenaga Kesehatan</option>
+              <option value="Fungsional Umum">Tenaga non Kesehatan</option>
+              <option value="Fungsional Tertentu">fungsional Tertentu</option>
+              <option value="WI/Dosen">Dosen Poltekkes Kemenkes</option>
+            </select>
+            <p className="text-red-600 text-sm italic">
+              Peneliti masuk dalam rumpung Non Kesehatan
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 pt-5 border-b pb-5 border-b-gray-600">
+          <h1 className="mx-5">Asal Daerah :</h1>
+          <div>
+            <div className="">
+              <input
+                id="default-radio-1"
+                type="radio"
+                value=""
+                name="default-radio"
+                className="w-4 h-4"
+              />
+              <label
+                htmlFor="default-radio-1"
+                className="ml-2 text-sm font-medium"
+              >
+                DTPK
+              </label>
+            </div>
+            <div className="">
+              <input
+                checked
+                id="default-radio-2"
+                type="radio"
+                value=""
+                name="default-radio"
+                className="w-4 h-4 "
+              />
+              <label
+                htmlFor="default-radio-2"
+                className="ml-2 text-sm font-medium"
+              >
+                Non DTPK
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 pt-5 border-b pb-5 border-b-gray-600">
+          <div className="mx-4">
+            <p>Golongan :</p>
+          </div>
+          <div className="mx-4">
+            <select
+              id="countries"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option value="">Jenis Pegawai</option>
+              <option value="Jabatan Struktural"> Tenaga Kesehatan</option>
+              <option value="Fungsional Umum">Tenaga non Kesehatan</option>
+              <option value="Fungsional Tertentu">fungsional Tertentu</option>
+              <option value="WI/Dosen">Dosen Poltekkes Kemenkes</option>
+            </select>
+            <p className="text-red-600 text-sm italic">
+              Peneliti masuk dalam rumpung Non Kesehatan
+            </p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 pt-5 border-b pb-5 border-b-gray-600">
+          <div className="mx-4">
+            <p>Jabatan :</p>
+          </div>
+          <div className="mx-4">
+            <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option value="">Pilih Jabatan</option>
+            </select>
+            <p className="text-red-600 text-sm italic"></p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 border-b border-b-gray-600 pt-4">
+          <p className="mx-5">No. SK PNS * :</p>
+          <div className="mt-1 grid mr-4">
+            <input
+              className="border mb-3 border-gray-400 md:w-64 placeholder-gray-300"
+              placeholder=" XXXXXXXXXXXXXX"
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 border-b border-b-gray-600 pt-4">
+          <p className="mx-5">Tanggal SK PNS * :</p>
+          <div className="mt-1 grid mr-4">
+            <input
+              className="border mb-3 border-gray-400 md:w-64 placeholder-gray-300"
+              placeholder=" XXXXXXXXXXXXXX"
+            />
+          </div>
+        </div>
+        <div>
+          <p className="bg-blue-600 text-white pl-5 font-semibold">
+            Data Pengusul
+          </p>
+          <div className="mx-4">
+            <div className="grid grid-cols-2 pt-5 border-b pb-5 border-b-gray-600">
+              <p>Status Kepegawaian :</p>
+              <div>
+                <select className="bg-gray-50 border md:w-64 w-full rounded-md p-2">
+                  <option value="">Kepesertaan</option>
+                  <option value="PNS">PNS Pusat</option>
+                </select>
+                <p className="text-red-600 text-sm italic"></p>
+              </div>
+            </div>
+          </div>
         </div>
       </form>
+      <div>
+        <button onClick={Alert}>Submit</button>
+      </div>
     </div>
   );
 };
